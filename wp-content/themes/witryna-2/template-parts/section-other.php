ae872c0f37440e1921affdfcd20cb858b4bb1141
vladislav.unterberg@solbeg.com <vladislav.unterberg@solbeg.com>
2021-02-02 14:28:49 +0300

    added new required files

---
+++ wp-content/themes/witryna-2/template-parts/section-other.php
@@ -0,0 +1,147 @@
+<section class="section section-bg">
+    <div class="center-wrapper">
+        <div class="section-headline">Baca juga</div>
+        <div class="line line_of-3">
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+        <div class="column_of-6">
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
+<section class="section decorated-bottom">
+    <div class="center-wrapper">
+        <div class="line line_of-4">
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+            <a href="#" class="card">
+                <div class="card-image">
+                    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2FEQjYeVYv86TI-kFJ0T4mu52NIKwfTz50Q&usqp=CAU">
+                </div>
+                <div class="content">
+                    <div class="title">Saat Liburan Bareng, Atta Dan Aurel Sekamar Atau Pisah?</div>
+                    <div class="post-date">
+                        <i class="far fa-calendar-alt"></i>
+                        <span class="date">13 oktober</span>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</section>
